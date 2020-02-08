64eebda8ffe89f405275ded675d0c6c9ce6ac54a
Grini Soufyane <grinisoufyan@outlook.com>
2020-02-08 12:52:40 +0100

    how to start new Activity

--- app/src/main/java/com/gridev/projectlinearylyout/HistoryActivity.java
+++ app/src/main/java/com/gridev/projectlinearylyout/HistoryActivity.java
@@ -3,6 +3,9 @@ package com.gridev.projectlinearylyout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.gridev.projectlinearylyout.HomeActivity.sentText;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        String getText = getIntent().getExtras().get(sentText).toString();
+
+        TextView text = findViewById(R.id.textviewhistory);
+        text.setTextSize(22);
+        text.setText("Welcome " + getText);
     }
 }
--- app/src/main/java/com/gridev/projectlinearylyout/HomeActivity.java
+++ app/src/main/java/com/gridev/projectlinearylyout/HomeActivity.java
@@ -2,13 +2,49 @@ package com.gridev.projectlinearylyout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String sentText = "sentText";
+
+    private ImageButton claculateMG;
+    private ImageButton historyMG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        claculateMG = findViewById(R.id.claculate_mg);
+        claculateMG.setOnClickListener(this);
+
+        historyMG = findViewById(R.id.histroy_mg);
+        historyMG.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.claculate_mg :
+                intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.histroy_mg :
+                intent = new Intent(HomeActivity.this, HistoryActivity.class);
+
+                intent.putExtra(sentText, "Grini Soufyane");
+
+                startActivity(intent);
+                break;
+
+        }
     }
 }
+
+
